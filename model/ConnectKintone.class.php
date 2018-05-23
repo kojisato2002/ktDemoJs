99a2457f96ce254f0d73c5e8eb95f59ed2150705
Koji Sato Sakura <cozy@cybozu-ri.co.jp>
2018-05-23 20:44:29 +0900

    Call API method implement

--- model/ConnectKintone.class.php
+++ model/ConnectKintone.class.php
@@ -5,26 +5,39 @@ class ConnectKintone {
   private $apiToken = '';
   private $subDomain ='';
   private $appNo = '';
-
-  public function getApiToken() {
-    return $this->apiToken;
-  }
+  
   public function setApiToken($_apiToken) {
     $this->apiToken = $_apiToken;
   }
+  public function getApiToken() { return $this->apiToken; }
 
-  public function getSubDomain() {
-    return $this->subDomain;
-  }
   public function setSubDomain($_subDomain) {
     $this->subDomain = $_subDomain;
   }
+  public function getSubDomain() { return $this->subDomain; }
 
-  public function getAppNo() {
-    return $this->appNo;
-  }
   public function setAppNo($_appNo) {
     $this->appNo = $_appNo;
+  }
+  public function getAppNo() { return $this->appNo; }
+
+  public function getRecords() {
+    //サーバ送信するHTTPヘッダを設定
+    $options = array(
+      'http'=>array(
+        'method'=>'GET',
+        'header'=> "X-Cybozu-API-Token:". $apiToken ."\r\n"
+       )
+    );
+    //コンテキストを生成
+    $context = stream_context_create( $options );
+    // サーバに接続してデータを貰う
+    $contents = file_get_contents( 'https://'. $subDomain .'.cybozu.com/k/v1/records.json?app='. $appNo , FALSE, $context );
+    
+    //JSON形式からArrayに変換
+    $data = json_decode($contents, true);
+    return $data;
+   
   }
 
 }
