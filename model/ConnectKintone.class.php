ab15c150491adb0a41d63edeb75a0a8c233abbe3
Koji Sato Sakura <cozy@cybozu-ri.co.jp>
2018-05-23 17:51:30 +0900

    Model directory added

---
+++ model/ConnectKintone.class.php
@@ -0,0 +1,32 @@
+<?php
+
+class ConnectKintone {
+
+  private $apiToken = '';
+  private $subDomain ='';
+  private $appNo = '';
+
+  public function getApiTokey() {
+    return $apiToken;
+  }
+  public function setApiToken($_apiToken) {
+    $apiToken = $_apiToken;
+  }
+
+  public function geSubDomain() {
+    return $subDomain;
+  }
+  public function setSubDomain($_subDomain) {
+    $subDomain = $_subDomain;
+  }
+
+  public function getAppNo() {
+    return $appNo;
+  }
+  public function setAppNo($_appNo) {
+    $appNo = $_addNo;
+  }
+
+}
+
+
